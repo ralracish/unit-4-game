74a8a1d15d7047046e15f7ebf4b5435deb899666
ralracish <carlarblackman@gmail.com>
2019-01-03 00:51:19 -0500

    updates to random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,78 @@
-
+$(document).ready(function(){
 // Global variables
 
-targetNumber=0
-randoms=0
-counter=0
+var targetNumber=0
+var randoms=[]
+var counter=0
+var wins=0
+var losses=0
 
 //To get target number
 
 //Create the function to start the game variables, but not actually run the function yet
-// function startGame() {
+function startGame() {
+  var targetNumber=0
+  var randoms=[]
+  var counter=0
 
-targetNumber=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+}
+
+var targetNumber=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
   
-console.log(targetNumber)
+console.log(targetNumber);
 
-var randoms = Array.from({length: 4}, () => Math.floor(Math.random() * 12));
+$("#number-to-guess").text("Number to Guess:" = targetNumber);
 
-console.log(randoms)
 
-counter=0
+// randoms = Array.from({length: 4}, () => Math.floor(Math.random() * 12));
+
+// console.log(randoms)
+    var crystalOne = Math.floor(Math.random() * 12) + 1; // creates crystal 1 value, a random number between 1-12
+    var crystalTwo = Math.floor(Math.random() * 12) + 1; // creates crystal 2 value, a random number between 1-12
+    var crystalThree = Math.floor(Math.random() * 12) + 1; // creates crystal 3 value, a random number between 1-12
+    var crystalFour = Math.floor(Math.random() * 12) + 1; // creates crystal 4 value, a random number between 1-12
+
+$(".crystal-image").on("click", function() {
+
+  // Determining the crystal's value requires us to extract the value from the data attribute.
+  // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+  // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+  // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+
+  var crystalValue = ($(this).attr("data-crystalvalue"));
+  crystalValue = parseInt(crystalValue);
+  // We then add the crystalValue to the user's "counter" which is a global variable.
+  // Every click, from every crystal adds to the global counter.
+  counter += crystalValue;
 
 
 
 
 // roundComplete() function
     // Here we will have all of the code that needs to be run after each guess is made.
-    function roundComplete() {
+    // function roundComplete() {
 
       // First, log an initial status update in the console
       // telling us how many wins and losses there are.
       console.log("wins" + youWin + " | losses " + youLose);
 
-       // Add to the win counter
-       youWin++;
-  
-       // Give the user an alert
-       alert("You win!");
+      
+    
+        // All of the same game win-lose logic applies. So the rest remains unchanged.
+        alert("New score: " + counter);
+    
+        if (counter === targetNumber) {
+          alert("You win!");
+          startGame();
+        }
+    
+        else if (counter >= targetNumber) {
+          alert("You lose!!");
+          startGame();
+        }
+      
    
-       // Update the win counter in the HTML
-       document.getElementById("wins").innerHTML = youWin;
-   
-       // Restart the game
-       startGame();
-     }
-   
-     // If we've score is greater than targetNumber
-     else if (guessesLeft === 0) {
-   
-       // Add to the loss counter
-       youLose++;
-   
-       // Give the user an alert
-       alert("You lose");
-   
-       // Update the loss counter in the HTML
-       document.getElementById("losses").innerHTML = youLose;
-   
-       // Restart the game
-       startGame();
-   
-     }
+    //  }
    
   //  }
    
@@ -67,15 +80,16 @@ counter=0
    // ==================================================================
    
    // Starts the Game by running the startGame() function
-   startGame();
+  //  startGame();
    
-   // Then initiates the function for capturing key clicks.
-   document.onkeyup = function(event) {
+   // Then initiates the function for capturing clicks.
+  //  
    
-     // Runs the code to check for correct guesses.
-     checkLetters(guessedLetters);
+     // Runs the code to add score
+        // addScore();
+    // 
    
      // Runs the code that ends each round.
-     roundComplete();
-   };
-// }
+    //  roundComplete();
+  //  };
+      })
