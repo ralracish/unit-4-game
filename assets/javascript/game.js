8331e725a35c6c1126948331a1b504c7a446f7bb
ralracish <carlarblackman@gmail.com>
2019-01-05 19:53:40 -0500

    final files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
+
 // Global variables
+var targetNumber;
+var crystalOne;
+var crystalTwo;
+var crystalThree;
+var crystalFour;
+var score=0
+var wins=0
+var losses=0
 
 //To get target number between 19 and 120
   var targetNumber=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
   console.log(targetNumber);
-  $("#number-to-guess").text("Number to Guess:  " + targetNumber)
+  $("#number-to-guess").text("Number to Guess:  " + targetNumber) //place Number to Guess on the screen
 
 //To generate random numbers for each crystal
   var crystalOne = Math.floor(Math.random() * 12) + 1; // creates crystal 1 value, a random number between 1-12
@@ -17,12 +26,13 @@ $(document).ready(function(){
   var score=0;
   var wins=0;
   var losses=0;
-  $("#wins").text("Wins: " + wins);
-  $("#losses").text("Losses: " + losses);
-  $("#wins").css("font-size", "30px");
-  $("#losses").css("font-size", "30px");
+  $("#wins").text("Wins: " + wins); //sets the Wins at 0 on the screen
+  $("#losses").text("Losses: " + losses); //sets the Losses at 0 on the screen
+  $("#wins").css("font-size", "30px"); //just trying to use jquery to style something as an extra bonus
+  $("#losses").css("font-size", "30px"); //just trying to use jquery to style something as an extra bonus
 
-  function reset(){
+  //Function to reset game 
+  function reset(){ 
   //To get new target number
   targetNumber=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     console.log(targetNumber);
@@ -40,15 +50,15 @@ $(document).ready(function(){
     
     wins=0;
     losses=0;
-
   }
+  
   function youWin(){
     wins++
     $("#wins").text("Wins: " + wins);
     alert("You win!");
     reset();
     console.log(wins);
-    }
+  }
 
   function youLose(){
     losses++
@@ -56,7 +66,7 @@ $(document).ready(function(){
     alert("You lose!!");
     reset();
     console.log(losses);
-   }
+  }
 
   $("#crystal-1").on ('click', function(){
     score = score + crystalOne;
@@ -80,7 +90,7 @@ $(document).ready(function(){
       else if (score > targetNumber) {
         youLose();
       }
-    })
+  })
 
   $("#crystal-3").on ('click', function(){
     score = score + crystalThree;
